cc68d6398ee85bcc6517dad6b6738b2ba0063be6
Anatolii Kalinin <anatoly.k2000@gmail.com>
2024-11-03 23:19:01 -0500

    fixed a typo

--- HW/HW#8/ROB501_HW8_P1.m
+++ HW/HW#8/ROB501_HW8_P1.m
@@ -16,15 +16,26 @@ Q = [1, 0.5, 0.5, 0.25;
     0.25, 1.0, 1.0, 4];
 
 % BLUE estimate of x using 2 values of y
-y_2 = y(1:2);
-x_hat_2 = inv(A(2,2)' * inv(Q(2,2)) * A(2,2)) * A(2,2)' * inv(Q(2,2)) * y_2
-covariance_m_2 = inv(A(2,2)' * inv(Q(2,2)) * A(2,2))
+[x_hat_2, covariance_m_2] = BLUE_estimate(A, y, Q, 2);
+x_hat_2
+covariance_m_2
 
 % BLUE estimate of x using 3 values of y
-y_3 = y(1:3);
-x_hat_3 = inv(A(3,2)' * inv(Q(3,3)) * A(3,2)) * A(3,2)' * inv(Q(3,3)) * y_3
-covariance_m_3 = inv(A(3,2)' * inv(Q(3,3)) * A(3,2))
+[x_hat_3, covariance_m_3] = BLUE_estimate(A, y, Q, 3);
+x_hat_3
+covariance_m_3
 
 % BLUE estimate of x using all values of y
-x_hat_all = inv(A' * inv(Q) * A) * A' * inv(Q) * y
-covariance_m_all = inv(A' * inv(Q) * A)
+[x_hat_all, covariance_m_all] = BLUE_estimate(A, y, Q, 4);
+x_hat_all
+covariance_m_all
+
+%% Compute BLUE estimate of x using n values of y
+function [x_hat, covariance_m] = BLUE_estimate(A, y, Q, n)
+y_n = y(1:n);
+A_n = A(1:n, :);
+Q_n = Q(1:n, 1:n);
+
+x_hat   = inv(A_n' * inv(Q_n) * A_n) * A_n' * inv(Q_n) * y_n;
+covariance_m = inv(A_n' * inv(Q_n) * A_n);
+end
