6fa723f231f8d7dd7984f08ed362b13b72b442c4
Anatolii Kalinin <anatoly.k2000@gmail.com>
2024-11-30 16:09:23 -0500

    Completed implementation of Newton-Raphson Method for HW#11, Problem 5

---
+++ HW/HW#11/ROB501_HW11_P5.m
@@ -0,0 +1,49 @@
+%% ROB501_HW11_P5
+% Implementing Newton-Raphson Method to solve F(x) = 0
+
+%% Define the Initial Guess and Solver Parameters
+x_initial = [-10; 10];
+eps = 1e-10; % Tolerance for convergence
+
+
+%% Solve using Newton-Raphson Method
+x_current = x_initial;
+d_norm = inf;
+
+% Newton-Raphson iteration
+while d_norm > eps
+    try
+        delta = dF(x_current) \ F(x_current);
+    catch
+        disp('Jacobian is singular, cannot proceed with Newton-Raphson method.');
+        break;
+    end
+    d_norm = norm(delta);
+    x_current = x_current - delta;
+end
+
+%% Show the Results
+disp('Initial guess = ');
+disp(x_initial);
+disp('F(x) = ');
+disp(F(x_current));
+disp('x = ');
+disp(x_current);
+
+%% Function Definitions
+
+% Function to compute F(x)
+function F_val = F(x)
+x1 = x(1);
+x2 = x(2);
+F_val = [3 + 4*x1 + 3*x2 - x1^2 - 2*x1*x2;
+    4 + 2*x1 + x2 - x1*x2 - 2*x2^2];
+end
+
+% Function to compute the Jacobian matrix of F(x)
+function dF_val = dF(x)
+x1 = x(1);
+x2 = x(2);
+dF_val = [4 - 2*x1 - 2*x2, 3 - 2*x1;
+    2 - x2, 1 - x1 - 4*x2];
+end
